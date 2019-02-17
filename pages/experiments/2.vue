37cbfcb59f5ba0775895882c057ba31a90c70463
flakessp <flakessp@gmail.com>
2019-02-17 23:22:36 +0300

    planning

--- pages/experiments/2.vue
+++ pages/experiments/2.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="container">
-    <div class="text-size-small position-overtop">launch party</div>
-    <div class="text-size-middle lineheight-small font-bold">
-      <div v-for="i in 3" :key=i >obsessions gallery</div>
-    </div>
+    <div class="slide">
+      <div class="text-size-small position-overtop">launch party</div>
+      <div class="text-size-middle lineheight-small font-bold">
+        <div v-for="i in 1" :key=i >знакомство с инфраструктурой</div>
+      </div>
+      
+      <div class="launchparty">
+        <div class="text-size-small font-serif">magazine<br>launch party</div>
+      </div>
     
-    <div class="launchparty">
-      <div class="text-size-small font-serif">magazine<br>launch party</div>
+      <div class="lineheight-negative">
+        <div v-for="i in 4" :key=i>mouthwash x <span class="font-italics">peerscape</span></div>
+      </div>
+    
+      <div class="booze">boooooooze</div>
+      <div class="text-size-big">march 31st</div>
+      <div class="text-size-big">7pm-10pm</div>
+      <div class="text-size-small">Congruent Space<br>1216 W Grand Ave, Chicago, IL</div>
     </div>
-  
 
-    <div class="lineheight-negative">
-      <div v-for="i in 4" :key=i>mouthwash x <span class="font-italics">peerscape</span></div>
+    <div class="slide">
+      <div class="text-size-middle lineheight-small font-bold">
+        <div v-for="i in 2" :key=i >VS Code</div>
+      </div>
     </div>
-   
-    <div class="booze">boooooooze</div>
-    <div class="text-size-big">march 31st</div>
-    <div class="text-size-big">7pm-10pm</div>
-    <div class="text-size-small">Congruent Space<br>1216 W Grand Ave, Chicago, IL</div>
+
+    <div class="slide">
+      <div class="text-size-middle lineheight-small font-bold">
+        <div v-for="i in 3" :key=i >Git</div>
+      </div>
+    </div>
+
+    <div class="slide">
+      <div class="text-size-middle lineheight-small font-bold">
+        <div v-for="i in 2" :key=i >VS Code</div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -30,11 +50,15 @@ export default {
 <style scoped>
 .container {
   height: 100vh;
-  padding: 2em;
   background: black;
   color: #EEE210;
   font-family: 'Aeroport', sans-serif;
   font-size: 50px;
+}
+.slide {
+  height: 100vh;
+  background: black;
+  padding: 2em;
 }
 
 .position-overtop {
---
+++ pages/splitscan/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div ref="canvas"></div>
+  </div>
+</template>
+<script>
+
+// import p5 from 'p5';
+
+// import "p5/lib/addons/p5.dom";
+export default {
+  data: function () {
+    return {
+      script: null,
+      ps: null,
+      x: 0,
+      y: 0,
+      canvas: null,
+      video: null
+    }
+  },
+  mounted () {
+    this.script = p => {
+      this.x = 100
+      this.y = 100
+
+      p.setup = () => {
+        this.canvas = p.createCanvas(1600, 480);
+          p.pixelDensity(1);
+          this.video = p.createCapture(p.VIDEO);
+          this.video.size(640,480);
+          p.background(255);
+        this.canvas.parent(this.$refs.canvas)
+      }
+
+      p.draw = () => {
+        p.background(0)
+        p.fill(255)
+        p.rect(this.x, this.y, 50, 50)
+      }
+    }
+    const P5 = require('p5')
+    // require("p5/lib/addons/p5.dom"); ?
+    this.ps = new P5(this.script)
+  }
+}
+</script>
+
+<style scoped>
+ .container {
+      height: 100vh;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+      justify-content: space-evenly;
+      align-items: center;
+      margin: 0;
+    }
+    canvas,video {
+      border: 10px solid black;
+    }
+</style>
