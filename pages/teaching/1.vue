fe2c3584d31f0f4cddd20d1c54d811546bc00165
flakessp <flakessp@gmail.com>
2019-02-22 18:12:06 +0300

    2-3

---
+++ pages/teaching/1.vue
@@ -0,0 +1,5 @@
+<template>
+<div class="container">
+  страница для первого курса
+</div>
+</template>
---
+++ pages/teaching/3.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <!-- GIT -->
+    <!-- продолжаем делать  -->
+  </div>
+</template>
