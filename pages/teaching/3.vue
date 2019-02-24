79138e6ced3cd5923a3ea2b9036d16afd97dcc79
flakessp <flakessp@gmail.com>
2019-02-25 00:53:59 +0300

    3е занятие

--- pages/teaching/3.vue
+++ pages/teaching/3.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="outer">
-  <div class="container">
+  <div class="container" >
     <!-- вспоминаем что выучили -->
     <!-- - шрифты -->
     <!-- основные html элементы -->
@@ -25,39 +25,75 @@
       <section>
         <h1>CSS HARD PARTS</h1>
         <h2>часть первая</h2>
-        <div class="logo">hashslash</div>
-        <div class="circle"></div>
+        <!-- <div class="logo">hashslash</div> -->
+        <div class="circle circle-centered"></div>
         <p></p>
       </section>
     </div>
     <div class="slide slide-2" :class="{isSelected: selected == 2}" @click="setSelected(2)">
+      <div class="caption">в предыдущей серии</div>
       <section>
-        <h1>в предыдущей серии</h1>
-        <h2>шрифты</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quas ipsam eum molestias mollitia deserunt repudiandae dignissimos laborum unde commodi? Eaque blanditiis obcaecati iste quis exercitationem illum, aut natus eligendi.</p>
+        <h1>На предыдущем занятии</h1>
 
-        <h2>основные html элементы</h2>
-        <p class="code-block">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius dolore pariatur dignissimos veniam nisi, alias sit dolores, repellendus repudiandae voluptas amet, sunt cumque quis nihil! Vero, vel. Libero, magnam nostrum.</p>
+        <h2>Тег,  Атрибут</h2>
+        <p>как выглядит тег, а как атрибут <br>какие бывают атрибуты (стили, классы, ссылки и тд)</p>
+
+        <h2>Основные html элементы</h2>
+        <p>
+          <ul>
+            <li>div</li>
+            <li>h1</li>
+            <li>p</li>
+            <li>br</li>
+            <li>span</li>
+            <li>a</li>
+            <li>img</li>
+            <li>em</li>
+            <li>b</li>
+          </ul>
+        </p>
         
-        <h2>тег,  атрибут</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quae quo temporibus odio labore asperiores, vero repudiandae iusto eveniet aliquam laudantium reprehenderit corporis placeat repellat sapiente est a perspiciatis nulla!</p>
-
         <h2>CSS</h2>
 
         <h2>Варианты записи CSS</h2>
 
-        <h2>основные свойства</h2>
+        <h2>Основные свойства</h2>
+        <!--  color, background-->
 
-        <h2>отступы внешние vs внутренние</h2>
+        <h2>Шрифты</h2>
+        <p>подключение, css свойства, ссылка на google fonts</p>
+        <p>
+          <ul>
+            <li>@font-face</li>
+              <li>font-family</li>
+            <li>font-size</li>
+            <li>font-weight</li>
+            <li>line-height</li>
+            <li>text-transform</li>
+          </ul>
+        </p>
+
       </section>
     </div>
     <div class="slide slide-3" :class="{isSelected: selected == 3}" @click="setSelected(3)">
+      <div class="caption">Box-model</div>
       <section>
         <h1>Box-model</h1>
+        <p>блочная модель расчитывает положение элемента на странице из следующиех свойств:</p>
         <h2>width, height</h2>
+        <p>высота и ширина, для линейных элементов не работает</p>
         <h2>padding</h2>
+        <p>внутренний отступ</p>
         <h2>margin</h2>
+        <p>margin: auto, выравниванием по центру устаноавливая равные отступы слева и справа</p>
+        <!-- про выровнять по центру -->
         <h2>border</h2>
+        <h2>сокращенная форма записи для всего выше</h2>
+        <p>
+          в одну строчку и значения по часовой стрелке margin: 10px 10px 10px 10px <br>
+          тоже самое, что и margin: 10px <br>
+          а margin: 10px 20px тоже самое, что и margin: 10px 20px 10px 20px (пары слева и справа)
+        </p>
 
         <h2>линейные и блочные</h2>
         <p>коротко говоря, линейным ширину и высоту задать нельзы, отступ сверху и снизу тоже (высота линии, привет!) это текст</p>
@@ -67,19 +103,66 @@
       </section>
     </div>
     <div class="slide slide-4" :class="{isSelected: selected == 4}" @click="setSelected(4)">
-      4
+      <div class="caption">Background</div>
+      <section>
+        <h1>Background</h1>
+        <h2>color</h2>
+        <p>задает цвет фона</p>
+        <h2>image</h2>
+        <p>задает картинку через url()</p>
+        <h2>Size</h2>
+        <p>задает поведение картинки внутри элемента</p>
+        <h2>gradient</h2>
+        <p>позволяет сделать переход от одного блока к другому <br>почитать - https://developer.mozilla.org/en-US/docs/Web/CSS/linear-gradient</p>
+      </section>
     </div>
-    <div class="slide slide-3" :class="{isSelected: selected == 3}" @click="setSelected(3)">
-      3
+    <div class="slide slide-5" :class="{isSelected: selected == 5}" @click="setSelected(5)">
+      <div class="caption">Единицы измерения</div>
+      <section>
+        <h1>Единицы Измерения</h1>
+         <h2>px</h2>
+         <h2>%</h2>
+         <h2>Em, Rem</h2>
+         <h2>Vh, Vw</h2>
+
+         <h2>#000</h2>
+         <h2>black</h2>
+         <h2>rgb(0,0,0)</h2>
+         <h2>rgba(0,0,0,0)</h2>
+      </section>
     </div>
-    <div class="slide slide-3" :class="{isSelected: selected == 3}" @click="setSelected(3)">
-      3
+    <div class="slide slide-6" :class="{isSelected: selected == 6}" @click="setSelected(6)">
+      <div class="caption">::псевдо-селекторы</div>
+      <section>
+        <h1>::псевдо-селекторы</h1>
+        <p>классная история</p>
+        <h2>hover</h2>
+        <h2>selection</h2>
+
+        <p>ссылки на почитать</p>
+      </section>
     </div>
-    <div class="slide slide-3" :class="{isSelected: selected == 3}" @click="setSelected(3)">
-      3
+    <div class="slide slide-7" :class="{isSelected: selected == 7}" @click="setSelected(7)">
+      <div class="caption">Transitions</div>
+      <section>
+        <h1>Transitions</h1>
+        <p>all .3s ease-in-out</p>
+      </section>
     </div>
-    <div class="slide slide-3" :class="{isSelected: selected == 3}" @click="setSelected(3)">
-      3
+    <div class="slide slide-8" :class="{isSelected: selected == 8}" @click="setSelected(8)">
+      <div class="caption">cursor</div>
+      <section>
+        <h1>Cursor</h1>
+        <p>какие размеры и все такое и ссылка</p>
+        
+      </section>
+    </div>
+    <div class="slide slide-9" :class="{isSelected: selected == 9}" @click="setSelected(9)">
+      <div class="caption">ДЗ</div>
+      <section>
+        <h1>Спасибо за внимание</h1>
+        <h2>ДЗ</h2>
+      </section>
     </div>                    
   </div>
 </div>
@@ -98,7 +181,13 @@ export default {
     },
     setSelected: function(i) {
       this.selected = i;
+    },
+    onKey: function(e) {
+      if (e.key == 'ArrowLeft') this.selected--
+      else if(e.key == 'ArrowRight') this.selected++
     }
+  }, mounted: function() {
+    window.addEventListener('keydown', this.onKey)
   }
 }
 </script>
@@ -107,7 +196,7 @@ export default {
 <style lang="scss" scoped>
 
 .outer {
-  width: 110vw;
+  width: 100vw;
 }
 .container {
   height: 100vh;
@@ -125,14 +214,14 @@ section {
 }
 
 .slide {
-  flex-basis: 10%;
+  flex-basis: 1%;
   background: black;
   border: 1px solid white;
   flex-shrink: 1;
   flex-grow: 1;
-  transition: all .5s;
+  transition: all .6s;
   overflow: auto;
-  color: white;
+  color: transparent;
   font-size: 50px;
   overflow-x: hidden;
   overflow-y: auto;
@@ -142,25 +231,44 @@ section {
 
   &.isSelected {
     flex-basis: 100%;
+    color: white;
+    .caption {
+      transform: translateX(-200%);
+    }
+  }
+  .caption {
+    position: absolute;
+    writing-mode: tb;
+    left: 1em;
+    font-size: 0.6em;
+    // font-style: italic;
+    text-transform: uppercase;
+    font-weight: 800;
+    color: white;
+    transition: all .5s;
+    cursor: pointer;
   }
 
   h1 {
-    font-size: 2em;
+    font-size: 2.5em;
     margin-bottom: .4em;
     line-height: 1;
+    font-family: 'pt mono';
   }
   h2 {
-    font-size: .8em;
+    font-size: 1.5em;
     margin-bottom: .3em;
+    font-family: 'pt mono';
+    letter-spacing: 1px;
   }
   
   p {
-    font-size: .6em;
+    font-family: 'aeroport';
+    font-size: .8em;
     line-height: 1;
     margin-bottom: 1em;
   }
   .code-block {
-    background: gray;
     font-family: monospace;
     padding: .25em 0;
     padding-left: .5em;
@@ -171,6 +279,8 @@ section {
   background: linear-gradient(darkblue, darksalmon);
   background-size: 200% 200%;
   background-position:0% 0%;
+
+    color: white !important;
   &:hover {
     background-position:100% 100%;
   }
@@ -183,6 +293,9 @@ section {
     height: 200px;
     border-radius: 50%;
     background: linear-gradient( darksalmon,darkblue);
+    &-centered {
+      margin: 0 auto;
+    }
     // box-shadow: 0px 0px 20px darkblue;
   }
 }
