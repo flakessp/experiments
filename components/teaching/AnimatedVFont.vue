2e5724ce3218fbb971ad80f1afe85331e4529362
flakessp <flakessp@gmail.com>
2019-03-01 20:50:37 +0300

    animation page

---
+++ components/teaching/AnimatedVFont.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="animated">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props: ['animation'],
+  computed: {
+    animatedStyle() {
+      return {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/teaching/4.vue
+++ pages/teaching/4.vue
@@ -5,27 +5,42 @@
             <h1>let hashslash = web.html + web.css + web.js</h1> -->
             <p>
                 <span :style="vFontStyles">position </span> top 
-            animation <span :style="vFontStyles">absolute</span>  let   
-             right @keyframes left jquery relative from to 
-            javascript DOM click bottom hashlash;
+            animation <span :style="vFontStyles">absolute</span>  <span>  #/</span>   
+             right @keyframes alternate  bottom jquery relative from to 
+            javascript DOM click left inifinite <span>hashlash;</span>
             </p>
+            <!-- <AnimatedFont class="animated">привет</AnimatedFont> -->
             
         </div>
     </div>
 </template>
 
 <script>
+import AnimatedFont from '@/components/teaching/AnimatedVFont';
+
 export default {
+    components: {AnimatedFont},
     data() {
         return {
-            rangeVal: 200
+            rangeVal: 0, 
+            animation: {
+                first: {
+                    color: {
+                        from: 'red',
+                        to: 'blue'
+                    },
+                    time: {
+                        duration: '1s',
+                        delay: '0s'
+                    }
+                }
+            }
         }
     },
     computed: {
         vFontStyles: function() {
             return {
-                fontVariationSettings: "'wght' " + this.rangeVal,
-                color: 'red'
+                fontVariationSettings: "'wght' " + this.rangeVal
             }
         }
     }
@@ -34,23 +49,33 @@ export default {
 
 
 <style lang="scss" scoped>
+
 $basic: #4B4B4B;
+
 @font-face {
   font-family: "Marvin Visions";
-  src: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1147877/MarvinVisionsGX-subset.woff2)
-    format("woff2");
+  src: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1147877/MarvinVisionsGX-subset.woff2) format("woff2");
   font-style: normal;
 }
+
 p {
     text-align: center;
     max-width: 60vw;
     line-height: .75;
+
     &::selection {
         background: red;
     }
 }
-span {
-    animation: color-size 3s forwards;
+
+.animated {
+    animation: color-size 3s alternate infinite;
+    
+    @for $i from 1 through 4 {
+        &:nth-child(#{$i}) {
+            animation-delay: #{$i * 0.2s};
+        }
+    }
 }
 
 .container {
@@ -60,10 +85,9 @@ span {
     justify-content: center;
     align-items: center;
     -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     height: 100vh;
     font-family: "Marvin Visions";
-    font-size: 11vmin;
+    font-size: 9vmin;
     font-variation-settings: "wght" 0
 }
 
@@ -71,17 +95,19 @@ h1 {
     font-size: .5em;
     font-variation-settings: "wght" 0;
     line-height:.8;
-      animation: size 10s alternate infinite;
+    
+    animation: size 10s alternate infinite;
 }
 
 @keyframes size {
-  from {
-    font-variation-settings: "wght" 00, "opsz" 80;
+    from {
+        font-variation-settings: "wght" 00;
+    }
+    to {
+        font-variation-settings: "wght" 170;
+    }
   }
-  to {
-    font-variation-settings: "wght" 170, "opsz" 80;
-  }
-}
+
 @keyframes color-size {
   from {
       color: $basic;
@@ -92,5 +118,6 @@ h1 {
       font-variation-settings: "wght" 170, "opsz" 80;
   }
 }
+
 </style>
 
